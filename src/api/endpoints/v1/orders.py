87f804448a60ba03485258b9d31e2f0e9ad2162b
Vinicio Schmidt <schmidtvinicio@gmail.com>
2023-03-17 16:41:53 -0300

    Add Depends on get_all_orders

--- src/api/endpoints/v1/orders.py
+++ src/api/endpoints/v1/orders.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from src.entities.orders.services import OrderService
+from src.shared.auth_bearer_validator import AuthBearerValidator
 
 orders_router = APIRouter()
 
 
 @orders_router.get("/orders/get_all_orders")
-async def get_all_orders():
+async def get_all_orders(token_infos: dict = Depends(AuthBearerValidator())):
     return OrderService.get_all_orders()
--- tests/unit/api/endpoints/v1/test_orders.py
+++ tests/unit/api/endpoints/v1/test_orders.py
@@ -1,6 +1,9 @@
+from fastapi import status
 import unittest
 from unittest.mock import patch
 from fastapi.testclient import TestClient
+
+from tests.utils.bearer_token_utils import JWTGenerator
 
 
 class TestOrdersV1(unittest.TestCase):
@@ -8,10 +11,21 @@ class TestOrdersV1(unittest.TestCase):
         from src.main import app
 
         self.client = TestClient(app)
+        self.jwt_generator = JWTGenerator()
 
-    @patch("src.api.endpoints.v1.orders.OrderService")
-    def test_get_all_orders(self, service_mock):
+    def test_get_all_orders_without_token(self):
         response = self.client.get("/v1/orders/get_all_orders")
 
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @patch("src.api.endpoints.v1.orders.OrderService")
+    def test_get_all_orders_with_valid_token(self, service_mock):
+        response = self.client.get(
+            "/v1/orders/get_all_orders",
+            headers={
+                "Authorization": f"Bearer {self.jwt_generator.generate_valid_token()}"
+            },
+        )
+
         service_mock.get_all_orders.assert_called_once()
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
