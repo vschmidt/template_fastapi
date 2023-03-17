f1829f969818522439ff7936f14c07712334cc57
Vinicio Schmidt <schmidtvinicio@gmail.com>
2023-03-17 12:02:04 -0300

    Add UserModel

---
+++ src/entities/users/models.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+
+from src.infrastructure.postgres.base import base
+
+class UserModel(base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer(), primary_key=True)
+    full_name = Column(String(240), nullable=False)
+    email = Column(String(240), nullable=False, unique=True)
+    cpf = Column(String(11), nullable=False, unique=True)
+    password = Column(String(240), nullable=False)
+    disabled = Column(Boolean(), nullable=False)
+    created_at = Column(DateTime(), default=datetime.now)
+    updated_at = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+    
+    def to_dict(self):
+        return dict([(k, getattr(self, k)) for k in self.__dict__.keys() if not k.startswith("_")])
--- src/infrastructure/postgres/metadata_agregation.py
+++ src/infrastructure/postgres/metadata_agregation.py
@@ -3,3 +3,5 @@
 class MetadataAggregation:
     def __init__(self):
         from src.entities.orders.models import OrderModel
+        from src.entities.users.models import UserModel
+        
