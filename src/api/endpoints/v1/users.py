ce7a406a712d44554fed07c8c9b2318ec94ff084
Vinicio Schmidt <schmidtvinicio@gmail.com>
2023-03-17 11:02:58 -0300

    Add UserRegister parameter

--- src/api/endpoints/v1/users.py
+++ src/api/endpoints/v1/users.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from src.entities.users.schemas import UserRegister
+
 users_router = APIRouter()
 
 @users_router.post("/users/create")
-async def get_all_orders():
+async def get_all_orders(user: UserRegister):
     pass
--- tests/unit/api/endpoints/v1/test_users.py
+++ tests/unit/api/endpoints/v1/test_users.py
@@ -2,12 +2,22 @@ import unittest
 from unittest.mock import patch
 from fastapi.testclient import TestClient    
 
+from src.entities.users.schemas import UserRegister
+
 class TestUsersV1(unittest.TestCase):
     def setUp(self):
         from src.main import app
         self.client = TestClient(app)
 
     def test_create_new_user(self):
-        response = self.client.post("/v1/users/create")
+        user = UserRegister(**{
+            "full_name": "Full Name",
+            "email": "email@email.com",
+            "cpf": "12312312312",
+            "password": "password",
+            "disabled": False
+        }).dict()
+
+        response = self.client.post("/v1/users/create", json=user)
 
         self.assertEqual(response.status_code, 200)
