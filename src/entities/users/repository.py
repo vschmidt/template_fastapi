c9fc99e49d455dfaeb0b482f553faf7ff0a481e5
Vinicio Schmidt <schmidtvinicio@gmail.com>
2023-03-17 13:57:09 -0300

    Ajust UserRepository

--- src/entities/users/repository.py
+++ src/entities/users/repository.py
@@ -1,21 +1,21 @@
 from sqlalchemy import insert, select
 
 from src.entities.users.models import UserModel
-from src.entities.users.schemas import UserRegister
+from src.entities.users.schemas import UserRegisterSchema
 from src.infrastructure.postgres.database import PostgresDatabase
 
 class UserRepository:
     @classmethod
-    def create_new_user(cls, user:UserRegister):
+    def create_new_user(cls, user:UserRegisterSchema):
         with PostgresDatabase() as engine:
-            engine.session.execute(insert(user))
+            engine.session.execute(insert(UserModel).values(**user.dict()))
             engine.session.commit()
 
     @classmethod
-    def get_user_by_cpf(cls, cpf:str) -> UserRegister:
+    def get_user_by_cpf(cls, cpf:str) -> UserRegisterSchema:
         with PostgresDatabase() as engine:
             result = engine.session.execute(select(UserModel).where(UserModel.cpf==cpf))
         
-        if result:
-            return UserRegister(**result.to_dict())        
+        if len(result.all())>0:
+            return UserRegisterSchema(**result[0].to_dict())        
         return None
