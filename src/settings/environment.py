5d92e43fd4563deb775f9b8b9dacdaf1c8989bda
Vinicio Schmidt <schmidtvinicio@gmail.com>
2023-03-16 21:53:22 -0300

    Update Environment defaults

--- src/settings/environment.py
+++ src/settings/environment.py
@@ -2,4 +2,4 @@ from pydantic import BaseSettings, Field
 
 
 class Environment(BaseSettings):
-    POSTGRES_CONNECTION_STRING: str = Field("postgresql+psycopg2://postgres:postgres@postgres/template_fastapi", env="POSTGRES_CONNECTION_STRING")
+    POSTGRES_CONNECTION_STRING: str = Field("postgresql+psycopg2://postgres:postgres@postgres/postgres", env="POSTGRES_CONNECTION_STRING")
