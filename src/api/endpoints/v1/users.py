f167905af048c3bca24fea467b24fff2c1d16214
Vinicio Schmidt <schmidtvinicio@gmail.com>
2023-03-17 11:46:56 -0300

    Add exception

--- src/api/endpoints/v1/users.py
+++ src/api/endpoints/v1/users.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+import logging
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
 
 from src.entities.users.schemas import UserRegister
 from src.entities.users.services import UserService
@@ -7,4 +9,11 @@ users_router = APIRouter()
 
 @users_router.post("/users/create")
 async def get_all_orders(user: UserRegister):
-    UserService.create_user(user)
+    try:
+        UserService.create_user(user)
+        return JSONResponse({"message":"Usuário criado"}, status_code=status.HTTP_200_OK)
+    except Exception as err:
+        logging.error(err)
+        return JSONResponse({"message":"Erro ao processar"}, status_code=status.HTTP_400_BAD_REQUEST)
+
+    
--- tests/unit/api/endpoints/v1/test_users.py
+++ tests/unit/api/endpoints/v1/test_users.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import patch
 from fastapi.testclient import TestClient    
+from fastapi import status
 
 from src.entities.users.schemas import UserRegister
 
@@ -10,7 +11,7 @@ class TestUsersV1(unittest.TestCase):
         self.client = TestClient(app)
     
     @patch("src.api.endpoints.v1.users.UserService")
-    def test_create_new_user(self, service_mock):
+    def test_create_new_user_with_succes(self, service_mock):
         user = UserRegister(**{
             "full_name": "Full Name",
             "email": "email@email.com",
@@ -22,4 +23,21 @@ class TestUsersV1(unittest.TestCase):
         response = self.client.post("/v1/users/create", json=user)
 
         self.assertEqual(response.status_code, 200)
+        service_mock.create_user.assert_called_once()
+
+    @patch("src.api.endpoints.v1.users.UserService")
+    def test_create_new_user_with_fail(self, service_mock):
+        user = UserRegister(**{
+            "full_name": "Full Name",
+            "email": "email@email.com",
+            "cpf": "12312312312",
+            "password": "password",
+            "disabled": False
+        }).dict()
+
+        service_mock.create_user.side_effect = ValueError
+
+        response = self.client.post("/v1/users/create", json=user)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         service_mock.create_user.assert_called_once()
